0864b519c95220b4bda5065a617c7d8dbe925e5f
Christian Rafael <christianparadix@gmail.com>
2025-11-23 23:20:15 -0300

    Initial commit

--- chrisai.py
+++ chrisai.py
@@ -1,4 +1,17 @@
 import torch
+import matplotlib
+matplotlib.use('TkAgg')
 from matplotlib import pyplot as plt
 import numpy as np
 import cv2
+import pyautogui
+
+model = torch.hub.load('yolov5', 'yolov5s', source='local')
+
+img = 'https://ultralytics.com/images/zidane.jpg'
+
+results = model(img)
+results.print()
+
+plt.imshow(np.squeeze(results.render()))
+plt.show()
